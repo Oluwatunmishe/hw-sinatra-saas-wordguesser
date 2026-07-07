73bb7885e8fdda0344b95cdc34166328f6b30541
Oluwatunmishe <73169340+Oluwatunmishe@users.noreply.github.com>
2026-07-07 15:46:39 -0700

    Add Gemfile.lock for Render deploy

---
+++ .zshrc
@@ -0,0 +1,4 @@
+export PATH="$PATH:/Applications/flutter/bin"
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init - zsh)"
+
--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -3,10 +3,54 @@ class WordGuesserGame
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  attr_accessor :word, :guesses, :wrong_guesses 
 
   def initialize(word)
+    
     @word = word
+    @guesses = []
+    @wrong_guesses = 0
+    
+    
   end
+  def check_win_or_lose 
+   if word_with_guesses == @word
+    :win
+   elsif @wrong_guesses >= 7
+    :lose
+   else
+    :play
+   end 
+  end 
+
+
+  def word_with_guesses
+    res = ""
+    @word.each_char do |letter|
+      if @guesses.include?(letter)
+        res << letter
+      else
+        res << "-"
+      end
+    end
+    res
+      
+  end 
+  def guess(letters)
+    
+    
+    if @guesses.include?(letters)
+        @wrong_guesses
+    elsif @word.include?(letters)
+        @guesses << letters
+    else
+      @wrong_guesses +=1
+    
+    end
+    @guesses
+  end
+
+
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
